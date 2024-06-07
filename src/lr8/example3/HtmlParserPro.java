8931629cab1130aabd910cec4c8b5571bc42e83a
vdmdrzdv <drovadim@mail.ru>
2024-06-08 01:37:30 +0500

    fix/lr8

--- src/lr8/example3/HtmlParserPro.java
+++ src/lr8/example3/HtmlParserPro.java
@@ -16,35 +16,39 @@ public class HtmlParserPro {
     public static void main(String[] args) {
         String fileName = "src/lr8/example3/news.txt";
         final String url = "http://fat.urfu.ru/index.html";
-        try (BufferedWriter file = new BufferedWriter(new FileWriter(fileName))) {
-            Connection.Response response = Jsoup.connect(url)
-                    .timeout(5000)
-                    .ignoreHttpErrors(true)
-                    .execute();
-            int statusCode = response.statusCode();
-            if (statusCode == 200) {
-                Document doc = Jsoup.connect(url).get();
-                Elements newsParent = doc.select("body > table > tbody > tr > td > div > table > " +
-                                "tbody > tr:nth-child(5) > td:nth-child(3) > table > tbody > tr > td:nth-child(1)");
-                for (int i = 3; i < 20; i++) {
-                    if (!(i % 2 == 0)) {
-                        List<Node> nodes = newsParent.get(0).childNodes();
-                        System.out.println("Тема: " + ((Element)nodes.get(i)).getElementsByClass("blocktitle")
-                                .get(0).childNodes().get(0));
-                        file.write("Тема: " + (((Element) nodes.get(i)).getElementsByClass("blocktitle")
-                                .get(0).childNodes().get(0) + "\n"));
+        for (int attempt = 0; attempt < 3; attempt++) {
+            try (BufferedWriter file = new BufferedWriter(new FileWriter(fileName))) {
+                Connection.Response response = Jsoup.connect(url)
+                        .timeout(5000)
+                        .ignoreHttpErrors(true)
+                        .execute();
+                int statusCode = response.statusCode();
+                if (statusCode == 200) {
+                    Document doc = Jsoup.connect(url).get();
+                    Elements newsParent = doc.select("body > table > tbody > tr > td > div > table > " +
+                            "tbody > tr:nth-child(5) > td:nth-child(3) > table > tbody > tr > td:nth-child(1)");
+                    for (int i = 3; i < 20; i++) {
+                        if (!(i % 2 == 0)) {
+                            List<Node> nodes = newsParent.get(0).childNodes();
+                            System.out.println("Тема: " + ((Element)nodes.get(i)).getElementsByClass("blocktitle")
+                                    .get(0).childNodes().get(0));
+                            file.write("Тема: " + (((Element) nodes.get(i)).getElementsByClass("blocktitle")
+                                    .get(0).childNodes().get(0) + "\n"));
 
-                        System.out.println("Дата: " + ((Element) nodes.get(i)).getElementsByClass("blockdate")
-                                .get(0).childNodes().get(0) + "\n");
-                        file.write("Дата: " + (((Element) nodes.get(i)).getElementsByClass("blockdate")
-                                .get(0).childNodes().get(0) + "\n\n"));
+                            System.out.println("Дата: " + ((Element) nodes.get(i)).getElementsByClass("blockdate")
+                                    .get(0).childNodes().get(0) + "\n");
+                            file.write("Дата: " + (((Element) nodes.get(i)).getElementsByClass("blockdate")
+                                    .get(0).childNodes().get(0) + "\n\n"));
+                        }
                     }
+                    break;
+                } else {
+                    System.out.println("Упс, что то пошло не так, код ошибки: " + statusCode);
+                    System.out.println("Попытка повторного подключения...");
                 }
-            } else {
-                System.out.println("Упс, что то пошло не так, код ошибки: " + statusCode);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
